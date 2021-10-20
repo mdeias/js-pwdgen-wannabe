0457f195812937c7a1c5879cd598c9f0aabc772c
mdeias <mdeias@icloud.com>
2021-10-20 16:12:29 +0200

    variabili

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
 
 // inizializzo variabili con dati
 // stampo dati aggiungendo 21
+
+const firstname = prompt ("Qual'è il tuo nome?");
+const surname = prompt ("Qual'è il tuo cognome?");
+const color = prompt ("Qual'è il tuo colore preferito?");
+
+console.log(firstname);
+console.log(surname);
+console.log(color);
+
