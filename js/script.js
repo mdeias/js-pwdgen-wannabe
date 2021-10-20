b7e001d88406420178649a83b8619f7256db5ae8
mdeias <mdeias@icloud.com>
2021-10-20 17:05:29 +0200

    Bonus

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@
 // output: nomecognomecolore21
 
 // inizializzo variabili con dati
-// stampo dati aggiungendo 21
+// creo variabile con tutti i dati aggiungendo 21
+// conto i caratteri
 
 const firstname = prompt ("Qual'è il tuo nome?");
 const surname = prompt ("Qual'è il tuo cognome?");
 const color = prompt ("Qual'è il tuo colore preferito?");
+const password = `${firstname}${surname}${color}21`; 
 
 console.log(firstname);
 console.log(surname);
 console.log(color);
+console.log(password);
 
 document.getElementById("dati").innerHTML =
 `
-Password: ${firstname}${surname}${color}21
-
+ ${firstname}${surname}${color}21 <br>
+Lunghezza: ${password.length} caratteri
 `;
